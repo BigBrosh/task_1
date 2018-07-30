6bac51a0164f6d2cbca3ed3c6371005f26e9f6b4
Big_Brosh <maksuhin@gmail.com>
2018-07-30 16:23:00 +0300

    router's bug fixed

--- src/components/Users.js
+++ src/components/Users.js
@@ -5,8 +5,7 @@ import {Link} from 'react-router-dom';
 export class Users extends React.Component {
 	state = {
 		users: this.props.data,
-		url: this.props.url,
-		active: 0
+		url: this.props.url
 	}
 
 	componentWillReceiveProps = (nextProps) => {
@@ -21,7 +20,7 @@ export class Users extends React.Component {
 			!this.state.users ?
 			<p>No users in the list</p> :
 			this.state.users.map((el, i) =>
-				<Link 	to={`users/nickname=${el.name}&id=${this.state.active + 1}`}
+				<Link 	to={`users/nickname=${el.name}&id=${el.id}`}
 						id={el.id}
 						key={`user${el.id}`}
 						onClick={this.getPosts}
