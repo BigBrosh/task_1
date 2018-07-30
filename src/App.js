79077c6126baa93aafe3408fd2862b85ec010db2
Big_Brosh <maksuhin@gmail.com>
2018-07-30 17:13:15 +0300

    App.js added

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import {Router, Route, Redirect} from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
+
+import MainPage from './views/MainPage';
+
+const history = createBrowserHistory();
+
+class App extends React.Component {
+	componentWillReceiveProps = nextProps =>{
+		if (nextProps)
+			return this.props.history !== nextProps.history;
+	};
+
+	render = () => {
+		return(
+			<Router history={history}>
+				<Route exact path="/" component={MainPage} />
+			</Router>
+		)
+	}
+};
+
+export default App;
